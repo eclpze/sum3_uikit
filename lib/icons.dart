314296ff505d822ca8fecc709c1c0724bcaa290a
Камилла Губайдуллина <gubai.k60@gmail.com>
2026-01-23 11:06:48 +0500

    Симуляция 3

---
+++ lib/icons.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class IconPackage {
+  static const String _fontFamily = 'CustomIcon';
+  static const IconData cart = IconData(0xe918, fontFamily: _fontFamily);
+
+}
