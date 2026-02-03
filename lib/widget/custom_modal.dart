d6335cd9c01c1d77c33356650140fd83d0343888
Камилла Губайдуллина <gubai.k60@gmail.com>
2026-02-03 10:41:41 +0500

    Симуляция 3

--- lib/widget/custom_modal.dart
+++ lib/widget/custom_modal.dart
@@ -6,9 +6,12 @@ import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:sum3_uikit/widget/custom_icon.dart';
 
 // Губайдуллина Камилла, 22.01.2026 18:22, модальное окно для показа информации
+enum ModalType { origin, photo }
+
 class CustomModal {
   static void show({
     required BuildContext context, // Контекст приложения
+    required ModalType typeModal, //
     required double borderRadiusModal, // Закругление формы модального окна
     required Color colorModal, // Цвет окна
     VoidCallback? onIcon, // Действие при нажатии на иконку
@@ -44,81 +47,97 @@ class CustomModal {
       ),
       backgroundColor: colorModal,
       clipBehavior: Clip.antiAlias,
-      builder: (context) => Container(
-        padding: EdgeInsets.all(padding),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: widthTitle,
-                  child: Text(title!, style: title2Semibold),
-                ),
-                Spacer(),
-                if (showIcon)
-                CustomIcon(
-                  padding: 0,
-                  onIcon: onIcon,
-                  widthIcon: widthIcon!,
-                  heightIcon: heightIcon!,
-                  pathIcon: pathIcon,
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            GestureDetector(
-              onTap: onText1,
-              child: Text(
-                text1,
-                style: headlineMedium.copyWith(color: caption),
-              ),
-            ),
-            SizedBox(height: 10),
-            if (text2 != null)
-              GestureDetector(
-                onTap: onText2,
-                child: Text(text2, style: textRegular),
-              ),
-            SizedBox(height: 40),
-            if (text3 != null)
-              Text(text3, style: headlineMedium.copyWith(color: caption)),
-            SizedBox(height: 10),
-            if (text4 != null) Text(text4, style: textRegular),
-            SizedBox(height: 20),
-            if (hasButton &&
-                textButton != null &&
-                widthButton != null &&
-                heightButton != null &&
-                colorText != null &&
-                colorButton != null &&
-                borderRadius != null &&
-                onPressed != null &&
-                paddingButton != null &&
-                type != null)
-              Row(
+      builder: (context) => (typeModal == ModalType.origin)
+          ? Container(
+              padding: EdgeInsets.all(padding),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  Spacer(),
-                  CustomButton(
-                    textButton: textButton,
-                    widthButton: widthButton,
-                    heightButton: heightButton,
-                    colorButton: colorButton,
-                    colorText: colorText,
-                    borderRadius: borderRadius,
-                    onPressed: onPressed,
-                    padding: paddingButton,
-                    type: type,
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: widthTitle,
+                        child: Text(title!, style: title2Semibold),
+                      ),
+                      Spacer(),
+                      if (showIcon)
+                        CustomIcon(
+                          padding: 0,
+                          onIcon: onIcon,
+                          widthIcon: widthIcon!,
+                          heightIcon: heightIcon!,
+                          pathIcon: pathIcon,
+                        ),
+                    ],
                   ),
-                  Spacer(),
+                  SizedBox(height: 20),
+                  Text(text1, style: headlineMedium.copyWith(color: caption)),
+                  SizedBox(height: 10),
+                  if (text2 != null) Text(text2, style: textRegular),
+
+                  SizedBox(height: 40),
+                  if (text3 != null)
+                    Text(text3, style: headlineMedium.copyWith(color: caption)),
+                  SizedBox(height: 10),
+                  if (text4 != null) Text(text4, style: textRegular),
+                  SizedBox(height: 20),
+                  if (hasButton &&
+                      textButton != null &&
+                      widthButton != null &&
+                      heightButton != null &&
+                      colorText != null &&
+                      colorButton != null &&
+                      borderRadius != null &&
+                      onPressed != null &&
+                      paddingButton != null &&
+                      type != null)
+                    Row(
+                      children: [
+                        Spacer(),
+                        CustomButton(
+                          textButton: textButton,
+                          widthButton: widthButton,
+                          heightButton: heightButton,
+                          colorButton: colorButton,
+                          colorText: colorText,
+                          borderRadius: borderRadius,
+                          onPressed: onPressed,
+                          padding: paddingButton,
+                          type: type,
+                        ),
+                        Spacer(),
+                      ],
+                    ),
                 ],
               ),
-          ],
-        ),
-      ),
+            )
+          : Container(
+              padding: EdgeInsets.all(padding),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(height: 10),
+                  GestureDetector(
+                    onTap: onText1,
+                    child: Text(
+                      text1,
+                      style: headlineMedium.copyWith(color: caption),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  if (text2 != null)
+                    GestureDetector(
+                      onTap: onText2,
+                      child: Text(text2, style: textRegular),
+                    ),
+                ],
+              ),
+            ),
     );
   }
 }
